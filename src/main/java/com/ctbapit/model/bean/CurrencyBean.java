26e62eb360700c989c9f78955bffec3118c6ea60
nkchiu <nkchiu418@gmail.com>
2021-07-28 15:31:08 +0800

    add currencybean

---
+++ src/main/java/com/ctbapit/model/bean/CurrencyBean.java
@@ -0,0 +1,27 @@
+package com.ctbapit.model.bean;
+
+
+public class CurrencyBean {
+	
+	private String code;
+	private String codeChn;
+	
+	public CurrencyBean() {}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCodeChn() {
+		return codeChn;
+	}
+
+	public void setCodeChn(String codeChn) {
+		this.codeChn = codeChn;
+	}
+	
+}
