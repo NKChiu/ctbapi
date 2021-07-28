24835a6b82c0b8a070a033d7050c3286a6502e9e
nkchiu <nkchiu418@gmail.com>
2021-07-28 11:06:35 +0800

    add getCoinDeskApi UnitTest

---
+++ src/test/java/com/ctbapit/cointest/CoinTest.java
@@ -0,0 +1,28 @@
+package com.ctbapit.cointest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ctbapit.CtbapiApplication;
+import com.ctbapit.config.UnitTestConfig;
+import com.ctbapit.model.vo.CurrentPriceVo;
+import com.ctbapit.service.ICoinService;
+import com.google.gson.Gson;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CtbapiApplication.class, UnitTestConfig.class})
+public class CoinTest {
+	
+	
+	@Autowired
+	private ICoinService coinService;
+		
+	@Test
+	public void getCoinDeskApi() {
+		CurrentPriceVo coinDeskApi = coinService.getCoinDeskApi();
+		System.out.println(new Gson().toJson(coinDeskApi));
+	}
+}
