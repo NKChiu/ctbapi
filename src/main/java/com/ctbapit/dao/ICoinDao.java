d1c169fff8bd3323892c2f533d12995681500297
nkchiu <nkchiu418@gmail.com>
2021-07-28 14:50:54 +0800

    modify coinDao

--- src/main/java/com/ctbapit/dao/ICoinDao.java
+++ src/main/java/com/ctbapit/dao/ICoinDao.java
@@ -1,5 +1,13 @@
 package com.ctbapit.dao;
 
-public interface ICoinDao {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.ctbapit.entity.CurrencyEntity;
+
+public interface ICoinDao extends CrudRepository<CurrencyEntity, Integer>{
+	
+	public CurrencyEntity findByCode(String code);
+	
 }
