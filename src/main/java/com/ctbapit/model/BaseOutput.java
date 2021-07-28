f09d842f747f3dccb8235ba4f28bb6f966bd6361
nkchiu <nkchiu418@gmail.com>
2021-07-28 14:48:01 +0800

    add baseOutput

---
+++ src/main/java/com/ctbapit/model/BaseOutput.java
@@ -0,0 +1,22 @@
+package com.ctbapit.model;
+
+public class BaseOutput {
+	
+	private boolean success;
+	private String retunrMessage;
+	
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getRetunrMessage() {
+		return retunrMessage;
+	}
+	public void setRetunrMessage(String retunrMessage) {
+		this.retunrMessage = retunrMessage;
+	}
+	
+	
+}
